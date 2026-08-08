856179279c3ac124e29a5db822e8b0f4768fce35
preethyvjohn-gif <preethyvjohn@gmail.com>
2026-08-08 21:00:07 +0930

    commit

---
+++ homework 2.py
@@ -0,0 +1,41 @@
+# Team points
+team1 = 50
+team2 = 40
+team3 = 60
+team4 = 30
+team5 = 70
+
+# Addition
+total = team1 + team2 + team3 + team4 + team5
+print("Total:", total)
+
+# Average
+average = total / 5
+print("Average:", average)
+
+# Power
+power = 2 ** 3
+print("Power:", power)
+
+# Floor division
+boxes = total // 10
+print("Full boxes:", boxes)
+
+# Modulus - leftover
+leftover = total % 10
+print("Leftover:", leftover)
+
+# Comparison
+last_week = 200
+
+print("Is this week more?", total > last_week)
+print("Is this week less?", total < last_week)
+print("Are they equal?", total == last_week)
+
+# Add bonus points
+total += 10
+print("After bonus:", total)
+
+# Subtract penalty points
+total -= 5
+print("After penalty:", total)
