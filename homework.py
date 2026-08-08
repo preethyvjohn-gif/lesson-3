26de197956aea6f9fa74b26fccd9b5eec9dd1289
preethyvjohn-gif <preethyvjohn@gmail.com>
2026-08-08 20:56:06 +0930

    commit

--- homework.py
+++ homework.py
@@ -1,8 +1,37 @@
-name = input("Enter your name: ")
-club = input("Enter your school club: ")
-member_number = 25     
-points = 89.5          
-event_count = 6       
-print("Badge number= 25")
-print("points = 89.5")
+name = input("What is your name? ")
+club = input("What is your school club? ")
 
+# Different data types
+member_number = 25
+points = 89.5
+events = 6
+hours = 10.5
+active = True
+
+# Changing data types
+points = int(points)
+events = float(events)
+member_number = str(member_number)
+active = str(active)
+
+# Show data types
+print(type(points))
+print(type(events))
+print(type(member_number))
+print(type(active))
+
+# Badge code
+badge_code = name[0:3] + name[-1]
+
+# Reverse club name
+secret_code = club[::-1]
+
+# Print badge
+print("Name: " + name)
+print("Club: " + club)
+print("Member Number: " + member_number)
+print("Points: " + str(points))
+print("Events: " + str(events))
+print("Active: " + active)
+print("Badge Code: " + badge_code)
+print("Secret Club Code: " + secret_code)
